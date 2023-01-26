4bebfa884665ea4ac496ad140018aa890f1240ac
EwertonSoares <ewerton.fa4@gmail.com>
2023-01-26 08:45:12 -0300

    Criação de de pacotes com arquitetura limpa

---
+++ projeto-donation/src/main/java/com/projetodonation/ProjetoDonationApplication.java
@@ -0,0 +1,14 @@
+package com.projetodonation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDonationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDonationApplication.class, args);
+
+	}
+
+}
---
+++ projeto-donation/src/test/java/com/projetodonation/ProjetoDonationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetodonation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDonationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
