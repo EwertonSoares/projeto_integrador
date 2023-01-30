61374b1dc5561a610f70ae5f50c2df67b10986dd
EwertonSoares <ewerton.fa4@gmail.com>
2023-01-30 15:10:47 -0300

    Ajustes

--- projeto-donation/src/main/java/com/projetodonation/controller/LoginController.java
+++ projeto-donation/src/main/java/com/projetodonation/controller/LoginController.java
@@ -5,15 +5,13 @@ import com.projetodonation.model.Usuario;
 import com.projetodonation.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@Controller
-@RequestMapping("/doacoes")
+@RestController
+@RequestMapping("/projeto-donation")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class LoginController {
     @Autowired
     private LoginService loginService;
