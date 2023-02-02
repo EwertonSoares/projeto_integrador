6df457432840c54c78bfa1e135bc2bd8eadad0c5
EwertonSoares <44639748+EwertonSoares@users.noreply.github.com>
2023-02-02 09:59:43 -0300

    Delete projeto-donation/src/main/java/com/projetodonation/service directory

--- projeto-donation/src/main/java/com/projetodonation/service/LoginService.java
+++
@@ -1,9 +0,0 @@
-package com.projetodonation.service;
-
-import com.projetodonation.controller.response.UsuarioResponse;
-import com.projetodonation.exceptions.NotFoundException;
-import com.projetodonation.model.Usuario;
-
-public interface LoginService {
-    UsuarioResponse logar(Usuario usuario) throws NotFoundException;
-}
