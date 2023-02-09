0b962c8b24890842b43431021c2ac650b1dc3ae8
Jackson Cardoso <jackson.oliveira@mercadolivre.com>
2023-02-09 14:33:44 -0300

    update basic security add route doacoes

--- projeto-donation/src/main/java/com/projetodonation/security/BasicSecurityConfig.java
+++ projeto-donation/src/main/java/com/projetodonation/security/BasicSecurityConfig.java
@@ -38,6 +38,7 @@ public class BasicSecurityConfig {
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/usuarios/logar").permitAll()
                         .requestMatchers("/usuarios/cadastrar").permitAll()
+                        .requestMatchers("/doacoes").permitAll()
                         .requestMatchers(HttpMethod.OPTIONS).permitAll()
                         .anyRequest().authenticated())
                 .httpBasic();
